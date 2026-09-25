f01fa9b66dfef463c1accf613ec1dd2541cd77d0
veadhanayaham veadha <veadhanayaahamnrk@gmail.com>
2026-09-25 21:55:51 +0530

    fix: set relative base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Option A: If your repo is named "portfolio", use '/portfolio/'
-  // Option B: To work with any repo name automatically, use './' (relative path)
   base: './',
 })
